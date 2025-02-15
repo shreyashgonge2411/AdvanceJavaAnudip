84e05454b2d1f3ba79187e343cd722a05cf33123
shreyashgonge2411 <shreyashgonge2411@gmail.com>
2025-02-15 16:32:16 +0530

    adding files to repo

---
+++ StudentManagement/src/main/java/com/student/Entity/Student.java
@@ -0,0 +1,28 @@
+package com.student.Entity;
+
+public class Student {
+	private long id;
+	private String name;
+	private int rollno;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	
+	
+}
